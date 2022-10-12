b16b35e8540a567f6f7212ee03023b836a7bbf8b
k-cem-sage <Kerim.Cem@sage.com>
2022-10-12 13:29:12 +0300

    FD-3494: svgs path is configured by env variables

--- components/AccountVerificationForm/AccountVerificationFormLearnMoreModal.js
+++ components/AccountVerificationForm/AccountVerificationFormLearnMoreModal.js
@@ -1,7 +1,6 @@
 import { useRef } from 'react';
 import { Button } from '../Button';
 import { Modal, ModalTitle } from '../Modal';
-import { assetPrefix } from "../next.config"
 
 export function AccountVerificationFormLearnMoreModal({ isOpen, onClose, onConfirm }) {
   const connectButtonRef = useRef(null);
@@ -22,7 +21,7 @@ export function AccountVerificationFormLearnMoreModal({ isOpen, onClose, onConfi
         </div>
         <div className="flex items-center justify-between">
           {/* Product logo - square */}
-          <img className="w-16 h-16" src={`${assetPrefix}product-logo-square.svg`} alt="Piper logo" />
+          <img className="w-16 h-16" src="/product-logo-square.svg" alt="Piper logo" />
           <div className="flex flex-col items-center justify-center space-y-1">
             {/* Icon: shield-check (outline) */}
             <svg
@@ -44,10 +43,10 @@ export function AccountVerificationFormLearnMoreModal({ isOpen, onClose, onConfi
               />
             </svg>
             {/* Basiq logo */}
-            <img className="w-16 h-4" src={`${assetPrefix}basiq-logo.svg`} alt="Basiq logo" />
+            <img className="w-16 h-4" src="/basiq-logo.svg" alt="Basiq logo" />
           </div>
           {/* Bank illustration */}
-          <img className="w-16 h-16" src={`${assetPrefix}bank-illustration.svg`} alt="Bank illustration" />
+          <img className="w-16 h-16" src="/bank-illustration.svg" alt="Bank illustration" />
         </div>
         {/* Dashed line - bottom */}
         <div className="px-8">
